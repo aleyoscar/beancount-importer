cf8d621e783a6289501327a4e80bef8399bb6009
aleyoscar <mail@aleyoscar.com>
2025-08-06 15:29:03 -0500

    feat(app): Validate ofx and ledger file paths

--- src/bean_import/bean_import.py
+++ src/bean_import/bean_import.py
@@ -1,21 +1,27 @@
 import typer
-from typing_extensions import Annotated
+from pathlib import Path
 from rich.console import Console
 from rich.theme import Theme
+from typing_extensions import Annotated
+
 theme = Theme({
     "file": "grey50",
     "error": "red"
 })
 
 def bean_import(
-    ofx: Annotated[str, typer.Argument(help="The ofx file to parse")],
-    ledger: Annotated[str, typer.Argument(help="The beancount ledger file to base the parser from")],
-    output: Annotated[str, typer.Option(help="The output file to write to instead of stdout", show_default=False)]="stdout"
+    ofx: Annotated[Path, typer.Argument(help="The ofx file to parse", exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True)],
+    ledger: Annotated[Path, typer.Argument(help="The beancount ledger file to base the parser from", exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True)],
+    output: Annotated[Path, typer.Option(help="The output file to write to instead of stdout", show_default=False, exists=False)]=None
 ):
     """
     Parse an OFX file based on a beancount LEDGER and output transaction entries to stdout
 
     Optionally specify an --output file
     """
+
     console = Console(theme=theme)
-    console.print(f"Parsing: [file]{ofx}[/] with [file]{ledger}[/] and outputting to [file]{output}[/]")
+    console_output = f"Parsing: [file]{ofx}[/] with [file]{ledger}[/]"
+    
+    if output: console_output +=  f"and outputting to [file]{output}[/]"
+    console.print(f"{console_output}")
